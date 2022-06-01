8e1dcda4652733ce2f129ae4543621bc7062db7a
FiruzM <madaminov.firuz@mail.ru>
2022-06-01 13:51:03 +0300

    Added slider for product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function(){
 
+    $(".product-slider__inner").slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        
+    });
 
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+      });
+      
     var mixer = mixitup('.product__inner-box');
 });
